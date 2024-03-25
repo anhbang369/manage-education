2169198a5d6c84e512ebea90f3a1848cc012a992
anhbang369 <damanhbangthpt@gmail.com>
2024-03-25 10:42:58 +0700

    impement login email with firebase

--- src/components/googleSignIn/Login.js
+++ src/components/googleSignIn/Login.js
@@ -1,29 +1,69 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { auth, provider } from "./firebase";
-import { signInWithPopup } from "firebase/auth";
-import SyllabusList from "../other/Others";
+import React, { useEffect, useState } from 'react';
+import { auth, provider } from './firebase';
+import { signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth';
+import SyllabusList from '../other/Others';
 
 const Login = () => {
-
-  const [value, setValue] = useState('')
-  const handleClick = () => {
-    signInWithPopup(auth, provider).then((data) => {
-      setValue(data.user.email)
-      localStorage.setItem("email", data.user.email)
-    })
-  }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    setValue(localStorage.getItem('email'))
-  })
+    const email = localStorage.getItem('email');
+    if (email) {
+      setUser(email);
+    }
+  }, []);
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const email = result.user.email;
+        localStorage.setItem('email', email);
+        setUser(email);
+      })
+      .catch((error) => {
+        console.error('Google Sign-in Error:', error);
+      });
+  };
+
+  const handleEmailSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const email = userCredential.user.email;
+        localStorage.setItem('email', email);
+        setUser(email);
+      })
+      .catch((error) => {
+        console.error('Email Sign-in Error:', error);
+      });
+  };
 
   return (
     <div>
-      {value ? <SyllabusList /> : <button onClick={handleClick}>Sign in google</button>}
-
+      {user ? (
+        <SyllabusList />
+      ) : (
+        <>
+          <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleEmailSignIn}>Sign in with Email</button>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
