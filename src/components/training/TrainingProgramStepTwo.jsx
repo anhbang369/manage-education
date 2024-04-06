74f27446e735de5413b45b46639cdf7ee6ffb110
anhbang369 <damanhbangthpt@gmail.com>
2024-04-06 10:20:33 +0700

    training program step two

--- src/components/training/TrainingProgramStepTwo.jsx
+++ src/components/training/TrainingProgramStepTwo.jsx
@@ -16,14 +16,81 @@ const TrainingProgramStepTwo = () => {
                 <p className='modified__content'>...days (...hours)</p>
                 <p className='modified__content'>Modified on 21/07/2023 by <b>Anh Bang</b></p>
                 <div className='modified__content-under'></div>
-                <h6>Content</h6>
+                <h6 className='content__header-prgram'>Content</h6>
+
+                {Data.map((item, index) => (
+                    <div className='syllabus_compo'>
+                        <div className='content__syllabus' key={index}>
+                            <div className='content__component'>
+                                <div className='content__com'><b>{item.title}</b></div>
+                                <div className='content__component-div'><p>{item.status}</p></div>
+                            </div>
+                            <div className='content__prgram'>
+                                <p className='content__prgram-p'>{item.programName}</p>
+                                <p className='content__prgram-p'>|</p>
+                                <p className='content__prgram-p'>{item.duration}</p>
+                                <p className='content__prgram-p'>|</p>
+                                <p className='content__prgram-p'>Modified on {item.modifiedDate} by {item.modifiedBy}</p>
+                            </div>
+                        </div>
+                        <div className='content__component-delete'>
+                            <i class="bi bi-trash"></i>
+                        </div>
+                    </div>
+
+                ))}
+
+
                 <div>
-                    <label htmlFor="">Select syllabus</label>
-                    <input />
+                    <label htmlFor=""><b>Select syllabus</b></label>
+                    <div className="input-with-icon">
+                        <i class="bi bi-search"></i>
+                        <input type="text" className="search__by" placeholder='Search by ...' />
+                    </div>
                 </div>
-            </div>
+
+                <div className="row save__general">
+                    <div className="col-md-1"><button className="general__pre">Back</button></div>
+                    <div className="col-md-7"></div>
+                    <div className="row col-md-4">
+                        <div className="col-md-6"></div>
+                        <div className="col-md-3"><button className="general__cancel"><a href="#" className="link__cancal">Cancal</a></button></div>
+                        <div className="col-md-2"><button className="general__next">Save</button></div>
+                    </div>
+                </div>
+            </div >
         </>
     )
 }
 
 export default TrainingProgramStepTwo
+
+const Data = [
+    {
+        id: 1,
+        title: "Linux",
+        status: "Active",
+        programName: "LIN v2.0",
+        duration: "4 days (12 hours)",
+        modifiedDate: "23/07/2024",
+        modifiedBy: "jonhy Deep"
+    },
+    {
+        id: 2,
+        title: "Linux",
+        status: "Active",
+        programName: "LIN v2.0",
+        duration: "4 days (12 hours)",
+        modifiedDate: "23/07/2024",
+        modifiedBy: "jonhy Deep"
+    },
+    {
+        id: 3,
+        title: "Linux",
+        status: "Active",
+        programName: "LIN v2.0",
+        duration: "4 days (12 hours)",
+        modifiedDate: "23/07/2024",
+        modifiedBy: "jonhy Deep"
+    }
+]
