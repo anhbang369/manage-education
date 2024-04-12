7bd56f20d89d34f292dd69d726e857e94febe023
anhbang369 <damanhbangthpt@gmail.com>
2024-04-12 11:54:36 +0700

    fix table view

--- src/Page/SyllabusList.js
+++ src/Page/SyllabusList.js
@@ -3,6 +3,9 @@ import Sidebar from '../components/sidebar/Sidebar';
 import Navbar from '../components/navbar/Navbar';
 import View from '../components/syllabus/View';
 import Footer from '../components/footer/Footer';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const SyllabusList = () => {
     return (
@@ -10,7 +13,14 @@ const SyllabusList = () => {
             <Navbar></Navbar>
             <div className='main'>
                 <Sidebar></Sidebar>
-                <View></View>
+                <React.Fragment>
+                    <CssBaseline />
+                    <Container fixed className='p-0 overflow-y-scroll'>
+                        <Box sx={{ bgcolor: '#cfe8fc', height: '100%', width: '100%' }}>
+                            <View />
+                        </ Box>
+                    </Container>
+                </React.Fragment>
             </div>
             <Footer></Footer>
         </>
