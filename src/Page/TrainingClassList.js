b05b2ed1749fe31de8d6116382d68b9e54c408b2
anhbang369 <damanhbangthpt@gmail.com>
2024-04-12 11:56:58 +0700

    fix all list system

--- src/Page/TrainingClassList.js
+++ src/Page/TrainingClassList.js
@@ -3,6 +3,9 @@ import Sidebar from '../components/sidebar/Sidebar';
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 import TrainingClasses from '../components/class/TrainingClassListView';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const TrainingClassList = () => {
     return (
@@ -10,7 +13,14 @@ const TrainingClassList = () => {
             <Navbar></Navbar>
             <div className='main'>
                 <Sidebar></Sidebar>
-                <TrainingClasses />
+                <React.Fragment>
+                    <CssBaseline />
+                    <Container fixed className='p-0 overflow-y-scroll'>
+                        <Box sx={{ bgcolor: '#cfe8fc', height: '100%', width: '100%' }}>
+                            <TrainingClasses />
+                        </ Box>
+                    </Container>
+                </React.Fragment>
             </div>
             <Footer></Footer>
         </>
--- src/Page/TrainingProgramList.js
+++ src/Page/TrainingProgramList.js
@@ -3,6 +3,9 @@ import Sidebar from '../components/sidebar/Sidebar';
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 import TrainingProgram from '../components/training/TrainingProgram';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const TrainingProgramList = () => {
     return (
@@ -10,7 +13,14 @@ const TrainingProgramList = () => {
             <Navbar></Navbar>
             <div className='main'>
                 <Sidebar></Sidebar>
-                <TrainingProgram></TrainingProgram>
+                <React.Fragment>
+                    <CssBaseline />
+                    <Container fixed className='p-0 overflow-y-scroll'>
+                        <Box sx={{ bgcolor: '#cfe8fc', height: '100%', width: '100%' }}>
+                            <TrainingProgram />
+                        </ Box>
+                    </Container>
+                </React.Fragment>
             </div>
             <Footer></Footer>
         </>
