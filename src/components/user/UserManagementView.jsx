f84bcb485514677b12c11b6735eb9ad0aebe753e
anhbang369 <damanhbangthpt@gmail.com>
2024-04-13 21:59:22 +0700

    get API create user update user

--- src/components/user/UserManagementView.jsx
+++ src/components/user/UserManagementView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import ActionMenu from '../action/ActionMenu';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import ReactPaginate from 'react-paginate';
 import ImportUser from '../import/ImportUser';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -14,9 +15,24 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { getUserData } from '../../services/UserService';
+import { getUserData, deActiveUser, deleteUser, getUserById, createUser } from '../../services/UserService';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+import Button from '@mui/joy/Button';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Modal from '@mui/joy/Modal';
+import ModalDialog from '@mui/joy/ModalDialog';
+import DialogTitle from '@mui/joy/DialogTitle';
+import Stack from '@mui/joy/Stack';
+import Select from '@mui/joy/Select';
+import MenuItem from '@mui/joy/Option';
 
 const UserManagementView = () => {
+
+    const [open, setOpen] = React.useState(false);
+    const [openAdd, setOpenAdd] = React.useState(false);
 
     //get list
     const [userData, setUserData] = useState(null);
@@ -34,6 +50,26 @@ const UserManagementView = () => {
 
         fetchData();
     }, []);
+
+    //get use by id
+    const [userIdData, setUserIdData] = useState({
+        fullName: '',
+        birthday: '',
+        gender: '',
+        email: '',
+        password: '',
+        level: ''
+    });
+
+    const handleGetUserById = async (itemId) => {
+        try {
+            const data = await getUserById(itemId);
+            console.log(data);
+            setUserIdData(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
 
     const getClassForStatus = (status) => {
@@ -67,6 +103,28 @@ const UserManagementView = () => {
             default:
                 return 'blueicon-bg';
         }
+    };
+
+    //notification
+    const [openNo, setOpenNo] = useState(false);
+    const [notificationMessage, setNotificationMessage] = useState('');
+    const handleCloseNo = () => {
+        setOpenNo(false);
+    };
+
+    const [selectedItemId, setSelectedItemId] = useState(null);
+    const handleDropdownItemClick = (itemId) => {
+        setSelectedItemId(itemId);
+        deActiveUser(itemId);
+        setNotificationMessage('De-active successful.');
+        setOpenNo(true);
+    };
+
+    const handleDropdownItemClickDelete = (itemId) => {
+        setSelectedItemId(itemId);
+        deleteUser(itemId);
+        setNotificationMessage('Delete successful.');
+        setOpenNo(true);
     };
 
     const [currentPage, setCurrentPage] = useState(0);
@@ -111,13 +169,82 @@ const UserManagementView = () => {
                                         </div>
                                     </div>
                                     <div>
-                                        <button className='text-white p-1 border-0 rounded bg-primary'><i class="bi bi-filter"></i>    <b>Filter</b></button>
+                                        <button className='text-white p-1 border-0 rounded bg-core'><i class="bi bi-filter"></i>    <b>Filter</b></button>
                                     </div>
                                 </div>
 
                                 <div className="col-md-3">
                                     <button className="border border-0 text-white rounded me-3 px-1 py-1 bg-warning" onClick={() => setImportOpen(true)}><i class="bi bi-cloud-upload"></i> Import</button>
-                                    <button className="border border-0 text-white rounded me-3 px-1 py-1 bg-core"><i class="bi bi-plus-circle"></i> Add new</button>
+                                    {/* <button className="border border-0 text-white rounded me-3 px-1 py-1 bg-core"><i class="bi bi-plus-circle"></i> Add new</button> */}
+                                    <React.Fragment>
+                                        <Button
+                                            backgroundColor="bg-core"
+                                            className="border border-0 text-white rounded me-3 px-1 bg-core"
+                                            onClick={() => setOpenAdd(true)}
+                                        >
+                                            <i class="bi bi-plus-circle"></i> Add new
+                                        </Button>
+                                        <Modal open={openAdd} onClose={() => setOpenAdd(false)}>
+                                            <ModalDialog>
+                                                <DialogTitle>Create new user</DialogTitle>
+                                                <form
+                                                    onSubmit={async (event) => {
+                                                        event.preventDefault();
+                                                        const formData = new FormData(event.target);
+                                                        const userData = Object.fromEntries(formData);
+
+                                                        try {
+                                                            await createUser(userData);
+
+                                                            console.log('User created successfully');
+                                                            setOpenAdd(true);
+                                                            setNotificationMessage('User created successfully.');
+                                                            setOpenNo(true);
+                                                        } catch (error) {
+                                                            console.log(error);
+                                                        }
+                                                    }}
+                                                >
+                                                    <Stack spacing={0}>
+                                                        <FormControl>
+                                                            <FormLabel>Name</FormLabel>
+                                                            <Input name="fullName" autoFocus required />
+                                                        </FormControl>
+                                                        <FormControl>
+                                                            <FormLabel>Birthday</FormLabel>
+                                                            <Input name="birthday" type='date' autoFocus required />
+                                                        </FormControl>
+                                                        <FormControl>
+                                                            <FormLabel>Gender</FormLabel>
+                                                            <Input name="gender" autoFocus required />
+                                                        </FormControl>
+                                                        <FormControl>
+                                                            <FormLabel>Email</FormLabel>
+                                                            <Input name="email" type='email' autoFocus required />
+                                                        </FormControl>
+                                                        <FormControl>
+                                                            <FormLabel>Password</FormLabel>
+                                                            <Input name="password" type='password' autoFocus required />
+                                                        </FormControl>
+                                                        <FormControl>
+                                                            <FormLabel>Role</FormLabel>
+                                                            <Select name="role" required placeholder="Choose one…">
+                                                                <MenuItem value="STUDENT">STUDENT</MenuItem>
+                                                                <MenuItem value="TRAINER">TRAINER</MenuItem>
+                                                                <MenuItem value="CLASS ADMIN">CLASS ADMIN</MenuItem>
+                                                            </Select>
+                                                        </FormControl>
+                                                        <FormControl>
+                                                            <FormLabel>Level</FormLabel>
+                                                            <Input name="level" required />
+                                                        </FormControl>
+                                                        <Button className='mt-2' type="submit">Submit</Button>
+                                                    </Stack>
+                                                </form>
+                                            </ModalDialog>
+                                        </Modal>
+                                    </React.Fragment>
+
                                 </div>
                             </div>
 
@@ -153,7 +280,74 @@ const UserManagementView = () => {
                                                 <TableCell align="left">{item.level}</TableCell>
                                                 <TableCell align="left"><p className={`syllabus_p mt-1 ${getClassForType(item.role.name)}`}>{item.role.name}</p></TableCell>
                                                 <TableCell align="left"><p className={`syllabus_p mt-1 ${getClassForStatus(item.status)}`}>{item.status}</p></TableCell>
-                                                <TableCell align="right"><ActionMenu></ActionMenu></TableCell>
+                                                <TableCell align="right">
+                                                    <div className="mb-2">
+                                                        {['start'].map(
+                                                            (direction) => (
+                                                                <DropdownButton
+                                                                    key={direction}
+                                                                    id={`dropdown-button-drop-${direction}`}
+                                                                    drop={direction}
+                                                                    variant="light-subtle"
+                                                                    title={<i className="bi bi-three-dots"></i>}
+                                                                    toggle={false}
+                                                                >
+                                                                    <Dropdown.Item eventKey="1" className='ms-1'><i className="bi bi-plus-circle"></i> Add training program</Dropdown.Item>
+                                                                    <Dropdown.Item eventKey="2" onClick={() => handleGetUserById(item.id)}>
+                                                                        <React.Fragment>
+                                                                            <Dropdown.Item eventKey="1" onClick={() => setOpen(true)} className='p-1'><i class="bi bi-pencil"></i> Update user</Dropdown.Item>
+                                                                            <Modal open={open} onClose={() => setOpen(false)}>
+                                                                                <ModalDialog>
+                                                                                    <DialogTitle>New user</DialogTitle>
+                                                                                    <form
+                                                                                        onSubmit={(event) => {
+                                                                                            event.preventDefault();
+                                                                                            setOpen(false);
+                                                                                        }}
+                                                                                    >
+                                                                                        <Stack spacing={0}>
+                                                                                            <FormControl>
+                                                                                                <FormLabel>Full Name</FormLabel>
+                                                                                                <Input value={userIdData.fullName} onChange={(event) => setUserIdData({ ...userIdData, fullName: event.target.value })} />
+                                                                                            </FormControl>
+                                                                                            <FormControl>
+                                                                                                <FormLabel>Birthday</FormLabel>
+                                                                                                <Input value={userIdData.birthday} onChange={(event) => setUserIdData({ ...userIdData, birthday: event.target.value })} />
+                                                                                            </FormControl>
+                                                                                            <FormControl>
+                                                                                                <FormLabel>Gender</FormLabel>
+                                                                                                <Input value={userIdData.gender} onChange={(event) => setUserIdData({ ...userIdData, gender: event.target.value })} />
+                                                                                            </FormControl>
+                                                                                            <FormControl>
+                                                                                                <FormLabel>Email</FormLabel>
+                                                                                                <Input value={userIdData.email} onChange={(event) => setUserIdData({ ...userIdData, email: event.target.value })} />
+                                                                                            </FormControl>
+                                                                                            <FormControl>
+                                                                                                <FormLabel>Level</FormLabel>
+                                                                                                <Input
+                                                                                                    value={userIdData.level}
+                                                                                                    onChange={(event) => setUserIdData({ ...userIdData, level: event.target.value })}
+                                                                                                />
+                                                                                            </FormControl>
+                                                                                            <Button type="submit" className='mt-3'>Submit</Button>
+                                                                                        </Stack>
+                                                                                    </form>
+                                                                                </ModalDialog>
+                                                                            </Modal>
+                                                                        </React.Fragment>
+                                                                    </Dropdown.Item>
+                                                                    <Dropdown.Item eventKey="3" onClick={() => handleDropdownItemClick(item.id)} className='ms-1'>
+                                                                        <i className="bi bi-copy"></i> De-activate user
+                                                                    </Dropdown.Item>
+                                                                    <Dropdown.Divider />
+                                                                    <Dropdown.Item eventKey="4" onClick={() => handleDropdownItemClickDelete(item.id)} className='ms-1'>
+                                                                        <i className="bi bi-trash3"></i> Delete syllabus
+                                                                    </Dropdown.Item>
+                                                                </DropdownButton>
+                                                            ),
+                                                        )}
+                                                    </div>
+                                                </TableCell>
                                             </TableRow>
                                         ))}
                                     </TableBody>
@@ -179,6 +373,16 @@ const UserManagementView = () => {
                                 renderOnZeroPageCount={null}
                             />
                             <ImportUser property={importOpen} />
+                            <Snackbar open={openNo} autoHideDuration={6000} onClose={handleCloseNo}>
+                                <Alert
+                                    onClose={handleCloseNo}
+                                    severity="success"
+                                    variant="filled"
+                                    sx={{ width: '100%' }}
+                                >
+                                    {notificationMessage}
+                                </Alert>
+                            </Snackbar>
                         </div>
                     </ Box>
                 </Container>
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -11,6 +11,19 @@ export const getUserData = async () => {
     }
 };
 
+export const getUserById = async (itemId) => {
+    try {
+        const response = await fetch(`http://localhost:8080/api/v1/auth/customer/${itemId}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch user data');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        throw error;
+    }
+};
+
 export const importUser = async (file) => {
     try {
         const formData = new FormData();
@@ -35,6 +48,78 @@ export const importUser = async (file) => {
         }
     } catch (error) {
         console.error('Error importing:', error);
+        throw error;
+    }
+};
+
+
+export const deActiveUser = async (itemId) => {
+    try {
+        const response = await fetch(`http://localhost:8080/api/v1/auth/customer/${itemId}/de-active`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': 'Bearer ' + "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkYW1hbmhiYW5nQGdtYWlsLmNvbSIsImlhdCI6MTcxMjk4MzI1NiwiZXhwIjoxNzEzMDY5NjU2fQ.jA8ijVU2w15aLVAed1x7b9aY_9ohuiL5W0mdQAq0XYA"
+            }
+        });
+
+        if (response.ok) {
+            await response;
+            console.log(response);
+            console.log(response.status);
+            return response;
+        } else {
+            console.error('Import failed');
+            throw new Error('Import failed');
+        }
+    } catch (error) {
+        console.error('Error importing:', error);
+        throw error;
+    }
+};
+
+
+export const deleteUser = async (itemId) => {
+    try {
+        const response = await fetch(`http://localhost:8080/api/v1/auth/customer/${itemId}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': 'Bearer ' + "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkYW1hbmhiYW5nQGdtYWlsLmNvbSIsImlhdCI6MTcxMjk4MzI1NiwiZXhwIjoxNzEzMDY5NjU2fQ.jA8ijVU2w15aLVAed1x7b9aY_9ohuiL5W0mdQAq0XYA"
+            }
+        });
+
+        if (response.ok) {
+            await response;
+            console.log(response);
+            console.log(response.status);
+            return response;
+        } else {
+            console.error('Import failed');
+            throw new Error('Import failed');
+        }
+    } catch (error) {
+        console.error('Error importing:', error);
+        throw error;
+    }
+};
+
+
+export const createUser = async (userData) => {
+    try {
+        const response = await fetch('http://localhost:8080/api/v1/auth/customer', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to create user');
+        }
+
+        return await response;
+    } catch (error) {
+        console.error('Error creating user:', error);
         throw error;
     }
 };
