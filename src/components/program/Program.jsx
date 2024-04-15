c7ace22933f61b28c3f319edcbab95d9f3dbc723
anhbang369 <damanhbangthpt@gmail.com>
2024-04-15 07:26:22 +0700

    match API training program completed

--- src/components/program/Program.jsx
+++ src/components/program/Program.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import "./program.css";
 import ActionMenuProgram from '../action/ActionMenuProgram';
 import TrainMaterial from '../trainMaterial/TrainMaterial';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 const Program = ({ programData }) => {
 
@@ -69,52 +71,77 @@ const Program = ({ programData }) => {
                                 <p className='ps-1 fw-normal'>Modified on {itemSyllabus.createdDate} by <b>{itemSyllabus.createdBy}</b></p>
                             </div>
 
-                            {itemSyllabus.syllabusDays && Array.isArray(itemSyllabus.syllabusDays) && itemSyllabus.syllabusDays.map((itemDay, indexDay) => (
-                                <div className="wrapper" key={indexDay}>
+                            {itemSyllabus.syllabusDays && Array.isArray(itemSyllabus.syllabusDays) && itemSyllabus.syllabusDays.map((day1, i) => (
+                                <div className="wrapper" key={i}>
                                     <div className='accordion accordion__wa'>
-                                        {itemDay.dayNo}
-                                        {itemDay.syllabusUnits && Array.isArray(itemDay.syllabusUnits) && itemSyllabus.syllabusUnits.map((day1, i) => (
-
-                                            <div className='item'>
-                                                <div className='title' onClick={() => toggle(i)}>
-                                                    <h6 className='outline__days'>{day1.dayNo}</h6>
-                                                </div>
-                                                <div className={selected === i ? 'content show' : 'content'}>
-                                                    {day1.syllabusUnits.map((unit, index) => (
-                                                        <div className="unit" key={index} onClick={() => toggles(i)}>
-                                                            <div className="unit__component">
-                                                                <div className='unit__com'>
-                                                                    <p className="unit__number">Unit{unit.unitNo}</p>
-                                                                    <div className='title__div'>
-                                                                        <p className="unit__title">{unit.name}</p>
-                                                                        <span className="unit__time">{unit.duration}</span>
-                                                                    </div>
-
-                                                                </div>
-                                                                <i className={selectedMore === i ? 'bi bi-caret-down-fill' : 'bi bi-caret-left-fill'}></i>
-                                                            </div>
-
-                                                            {unit.syllabusUnitChapters.map((detail, idx) => (
-                                                                <div className={selectedMore === i ? 'unit__details show' : 'unit__details'} key={idx}>
-                                                                    <h6 className='details__title'>{detail.name}</h6>
-                                                                    <p className='details__stanrd'>{detail.outputStandard}</p>
-                                                                    <p className='details__mins'>{detail.duration} mins</p>
-                                                                    {detail.deliveryType.name === 'Concept/Lecture' && <i class="bi bi-person-plus"></i>}
-                                                                    {detail.deliveryType.name === 'Assignment/Lab' && <i class="bi bi-bookmark-check"></i>}
-                                                                    {detail.deliveryType.name === 'Test/Quiz' && <i class="bi bi-card-checklist"></i>}
-                                                                    {detail.deliveryType.name === 'Exam' && <i class="bi bi-journal-bookmark-fill"></i>}
-                                                                    {detail.deliveryType.name === 'Guide/Review' && <i class="bi bi-hand-thumbs-up"></i>}
-                                                                    {detail.deliveryType.name === 'Seminar/Workshop' && <i class="bi bi-person-workspace"></i>}
-                                                                    {detail.deliveryType.name === 'Class Meeting' && <i class="bi bi-people"></i>}
-                                                                    {detail.deliveryType.name === 'Tour/Outdoor' && <i class="bi bi-globe-central-south-asia"></i>}
-                                                                    <i className={detail.icons[1]} onClick={() => setImportOpen(true)}></i>
-                                                                </div>
-                                                            ))}
-                                                        </div>
-                                                    ))}
-                                                </div>
+                                        <div className='item'>
+                                            <div className='title' onClick={() => toggle(i)}>
+                                                <h6 className='outline__days'>{day1.dayNo}</h6>
                                             </div>
-                                        ))}
+                                            <div className={selected === i ? 'content show' : 'content'}>
+                                                {day1.syllabusUnits.map((unit, index) => (
+                                                    <div className="unit" key={index} onClick={() => toggles(i)}>
+                                                        <div className="unit__component">
+                                                            <div className='d-flex mb-2'>
+                                                                <p className="unit__number">Unit {unit.unitNo}</p>
+                                                                <div className='ms-4'>
+                                                                    <p className="unit__title">{unit.name}</p>
+                                                                    <span className="unit__time fs-14">{unit.duration} hours</span>
+                                                                </div>
+
+                                                            </div>
+                                                            <i className={selectedMore === i ? 'bi bi-caret-down-fill' : 'bi bi-caret-left-fill'}></i>
+                                                        </div>
+
+                                                        {unit.syllabusUnitChapters.map((detail, idx) => (
+                                                            <div className={selectedMore === i ? 'unit__details show' : 'unit__details'} key={idx}>
+
+                                                                <Box sx={{ flexGrow: 1 }}>
+                                                                    <Grid container spacing={1}>
+                                                                        <Grid item xs={5}>
+                                                                            <h6 className='fs-14'>{detail.name}</h6>
+                                                                        </Grid>
+                                                                        <Grid item xs={1}>
+                                                                            <p className='details__stanrd'>
+                                                                                {detail.outputStandard == null ? (
+                                                                                    <p className='bg-chapter'></p>
+                                                                                ) : (
+                                                                                    <p className='details__stanrd'>{detail.outputStandard.code}</p>
+                                                                                )}
+                                                                            </p>
+                                                                        </Grid>
+                                                                        <Grid item xs={2}>
+                                                                            <p className='details__mins'>{detail.duration} mins</p>
+                                                                        </Grid>
+                                                                        <Grid item xs={2}>
+                                                                            <p>
+                                                                                {detail.online ? <p className='details__onl'>Online</p> : <p className='text-white bg-core rounded p-1 fw-normal'>Offline</p>}
+                                                                            </p>
+                                                                        </Grid>
+                                                                        <Grid item xs={1}>
+                                                                            {detail.deliveryType.name === 'Concept/Lecture' && <i class="bi bi-person-plus"></i>}
+                                                                            {detail.deliveryType.name === 'Assignment/Lab' && <i class="bi bi-bookmark-check"></i>}
+                                                                            {detail.deliveryType.name === 'Test/Quiz' && <i class="bi bi-card-checklist"></i>}
+                                                                            {detail.deliveryType.name === 'Exam' && <i class="bi bi-journal-bookmark-fill"></i>}
+                                                                            {detail.deliveryType.name === 'Guide/Review' && <i class="bi bi-hand-thumbs-up"></i>}
+                                                                            {detail.deliveryType.name === 'Seminar/Workshop' && <i class="bi bi-person-workspace"></i>}
+                                                                            {detail.deliveryType.name === 'Class Meeting' && <i class="bi bi-people"></i>}
+                                                                            {detail.deliveryType.name === 'Tour/Outdoor' && <i class="bi bi-globe-central-south-asia"></i>}
+
+                                                                        </Grid>
+                                                                        <Grid item xs={1}>
+                                                                            <i class="bi bi-folder2-open" onClick={() => setImportOpen(true)}></i>
+                                                                        </Grid>
+                                                                    </Grid>
+                                                                </Box>
+                                                                <TrainMaterial property={importOpen}></TrainMaterial>
+                                                            </div>
+                                                        ))}
+
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </div>
 
                                     </div>
                                 </div>
